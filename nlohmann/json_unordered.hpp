ef41531151296dffd02734cfbe044e726247c372
Vic P <vic4key@gmail.com>
2022-10-28 18:23:33 +0700

    Initial

---
+++ nlohmann/json_unordered.hpp
@@ -0,0 +1,13 @@
+#ifndef NLOHMANN_JSON_UNORDERED_H
+#define NLOHMANN_JSON_UNORDERED_H
+
+#include "nlohmann/json.hpp"
+#include "nlohmann/fifo_map.hpp"
+
+template<class K, class V, class dummy_compare, class A>
+using unordered_fifo_map = nlohmann::fifo_map<K, V, nlohmann::fifo_map_compare<K>, A>;
+using json = nlohmann::basic_json<unordered_fifo_map>;
+
+// using json = nlohmann::json; // ordered
+
+#endif  // NLOHMANN_JSON_UNORDERED_H
